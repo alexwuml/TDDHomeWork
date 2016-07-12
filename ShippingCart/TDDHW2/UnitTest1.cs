0afad248b405e0255b46c068c25ed78b2a7bb489
BearWu <BearWu@DESKTOP-JA1OFEM>
2016-07-12 17:26:17 +0800

    First test case for 一本書100元

---
+++ ShippingCart/TDDHW2/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TDDHW2
+{
+    [TestClass]
+    public class ShhippingCartTests
+    {
+        [TestMethod]
+        public void CalculateFee_1book_for_100()
+        {
+            //Arrange
+            var target = new ShippingCart();
+            List<Book> booklist =
+                new List<Book>
+            {
+                new Book {Name ="",Qty=1,UnitPrice=100 }
+            };
+
+            //Actual
+            double actual = target.CalculateFee(booklist);
+
+            //Assert
+            double expected = 100d;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
